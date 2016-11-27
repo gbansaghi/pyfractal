5432b887f95a212d2a18ef2890df11d1ed89a7e3
George Bansaghi <bansaghi@gmail.com>
2016-11-27 16:36:21 +0100

    added SparseMatrix class

---
+++ testing/test_sparsematrix.py
@@ -0,0 +1,126 @@
+import unittest
+from util import sparsematrix
+
+class SparseMatrixTest(unittest.TestCase):
+    def setUp(self):
+        self.rows = 2
+        self.columns = 2
+        self.matrix = sparsematrix.SparseMatrix(self.rows, self.columns)
+
+    def test_assignment(self):
+        assigned_value = 1
+        assigned_coord = (0, 0)
+        self.matrix[assigned_coord] = assigned_value
+        self.assertEqual(self.matrix[assigned_coord], assigned_value)
+
+    def test_addition(self):
+        assigned_value = 1
+        assigned_coord = (0, 0)
+        assignments = 2
+        for i in range(assignments):
+            self.matrix[assigned_coord] += assigned_value
+        self.assertEqual(self.matrix[assigned_coord],
+                         assigned_value*assignments)
+
+    def test_sparsity(self):
+        assignments = {(0, 0): 1,
+                       (0, 1): 2}
+        for coord, value in assignments.items():
+            self.matrix[coord] = value
+        self.assertEqual(len(self.matrix.keys()), len(assignments.keys()))
+
+    def test_addition_sparsity(self):
+        assignments = {(0, 0): 1,
+                       (0, 1): 2}
+        for coord, value in assignments.items():
+            for i in range(value):
+                self.matrix[coord] += value
+        self.assertEqual(len(self.matrix.keys()), len(assignments.keys()))
+
+    def test_str(self):
+        self.matrix[0, 0] = 1
+        self.matrix[0, 1] = 2
+        self.matrix[1, 0] = 3
+        self.matrix[1, 1] = 4
+        self.assertEqual(str(self.matrix), '1 2\n3 4')
+
+    def test_invalid_coord(self):
+        with self.assertRaises(KeyError):
+            self.matrix[-1, 0]
+        with self.assertRaises(KeyError):
+            self.matrix[0, -1]
+
+    def test_out_of_bounds(self):
+        with self.assertRaises(KeyError):
+            self.matrix[0, self.columns+1]
+        with self.assertRaises(KeyError):
+            self.matrix[self.rows+1, 0]
+
+
+class SparseMatrixOperationsTest(unittest.TestCase):
+    def setUp(self):
+        self.rows = 2
+        self.columns = 2
+
+        self.matrix = sparsematrix.SparseMatrix(self.rows, self.columns)
+        self.other  = sparsematrix.SparseMatrix(self.rows, self.columns)
+
+        self.other[0, 0] = 1
+        self.other[0, 1] = 2
+        self.other[1, 0] = 3
+        self.other[1, 1] = 4
+
+    def test_merge(self):
+        self.matrix.merge(self.other)
+
+        for row in range(self.rows):
+            for column in range(self.columns):
+                with self.subTest(row=row, column=column):
+                    self.assertEqual(self.matrix[row, column],
+                                     self.other[row, column])
+
+    def test_merge_row_offset(self):
+        self.matrix.merge(self.other, row_offset=1)
+
+        for row in range(self.rows):
+            for column in range(self.columns):
+                with self.subTest(row=row, column=column):
+                    if row == 0:
+                        self.assertEqual(self.matrix[row, column], 0)
+                    else:
+                        self.assertEqual(self.matrix[row, column],
+                                         self.other[row - 1, column])
+
+    def test_merge_column_offset(self):
+        self.matrix.merge(self.other, column_offset=1)
+
+        for row in range(self.rows):
+            for column in range(self.columns):
+                with self.subTest(row=row, column=column):
+                    if column == 0:
+                        self.assertEqual(self.matrix[row, column], 0)
+                    else:
+                        self.assertEqual(self.matrix[row, column],
+                                         self.other[row, column - 1])
+
+    def test_add(self):
+        result = self.matrix + self.other
+
+        for row in range(self.rows):
+            for column in range(self.columns):
+                with self.subTest(row=row, column=column):
+                    self.assertEqual(result[row, column],
+                                     self.matrix[row, column] \
+                                     + self.other[row, column])
+
+    def test_inplace_add(self):
+        original = sparsematrix.SparseMatrix(self.rows, self.columns)
+        original.add_entries(self.matrix.entries)
+        self.matrix += self.other
+
+        for row in range(self.rows):
+            for column in range(self.columns):
+                with self.subTest(row=row, column=column):
+                    self.assertEqual(self.matrix[row, column],
+                                     original[row, column] \
+                                     + self.other[row, column])
---
+++ util/sparsematrix.py
@@ -0,0 +1,91 @@
+def bounds_checked(func):
+    def wrapper(self, pos, *args, **kwargs):
+        self.bounds_check(pos)
+        return func(self, pos, *args, **kwargs)
+    return wrapper
+
+
+class SparseMatrix:
+    def __init__(self, rows, columns):
+        self.entries = dict()
+        self.rows = rows
+        self.columns = columns
+
+    def bounds_check(self, key):
+        if key[0] < 0 or key[1] < 0:
+            raise KeyError("matrix coordinates cannot be negative")
+        if key[0] > self.rows:
+            raise KeyError("row index "+str(key[0])+" out of bounds")
+        if key[1] > self.columns:
+            raise KeyError("column index "+str(key[1])+" out of bounds")
+
+    def keys(self):
+        return self.entries.keys()
+
+    def values(self):
+        return self.entries.values()
+
+    def items(self):
+        return self.entries.items()
+
+    @bounds_checked
+    def __getitem__(self, pos):
+        if pos not in self.entries:
+            return 0
+        return self.entries[pos]
+
+    @bounds_checked
+    def __setitem__(self, pos, value):
+        if value == 0:
+            self.entries.pop(pos, None)
+        else:
+            self.entries[pos] = value
+        return self
+
+    def __str__(self):
+        row_strings = []
+        try:
+            decimals = len(str(max(self.entries.values())))
+        except Exception:
+            decimals = 1
+        format_string='{{:{}d}}'.format(decimals)
+        for row in range(0, self.rows):
+            row_string = " ".join([format_string.format(self[row,i]) \
+                                   for i in range(self.columns)])
+            row_strings.append(row_string)
+        return '\n'.join(row_strings)
+
+    def merge(self, other, row_offset=0, column_offset=0):
+        # Transform entries
+        entries = {(row + row_offset, column + column_offset): value \
+                   for (row, column), value in other.entries.items()}
+
+        self.add_entries(entries)
+        return self
+
+    def add_entries(self, entries):
+        # Filter entries
+        filtered = {(row, column): value \
+                    for (row, column), value in entries.items() \
+                    if  row    < self.rows \
+                    and column < self.columns}
+
+        # Update existing entries
+        for key in set(self.entries.keys()) & set(filtered.keys()):
+            self.entries[key] += filtered[key]
+
+        # Add new entries
+        self.entries.update({key: filtered[key] \
+                             for key in \
+                             set(filtered.keys()) - set(self.entries.keys())})
+
+
+    def __add__(self, other):
+        result = SparseMatrix(self.rows, self.columns)
+        result.add_entries(self.entries)
+        result.add_entries(other.entries)
+        return result
+
+    def __iadd__(self, other):
+        self.add_entries(other.entries)
+        return self
